a2d0db94d195d5a1ba07dfe03fd9c318ad5c095d
Hollow1 <113149993+Ho11ow1@users.noreply.github.com>
2025-08-28 23:58:14 +0200

    Get base working grounds & config (#1)

--- AstroFrontend/astro.config.mjs
+++ AstroFrontend/astro.config.mjs
@@ -1,5 +1,25 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import dotenv from 'dotenv';
 
-// https://astro.build/config
-export default defineConfig({});
+dotenv.config({ path: '../.env' });
+
+export default defineConfig({
+    vite: 
+    {
+        resolve: 
+        {
+            alias: 
+            {
+                '@components': '/src/components',
+                '@data': '/src/data',
+                '@pages': '/src/pages',
+                '@styles': '/src/styles'
+            }
+        }
+    },
+    devOptions:
+    {
+        port: Number(process.env.ASTRO_PORT) || 3000,
+        strictPort: true
+    }
+});
