f3a06a83b0ad29d467a83a3ae255a27d57b30e8b
Hollow1 <jakubvalesilver@gmail.com>
2025-08-28 23:56:23 +0200

    Get base working grounds & config

---
+++ AstroFrontend/src/data/AstroDefs.ts
@@ -0,0 +1,21 @@
+const Backend: string = import.meta.env.ACTIVE_BACKEND.toLowerCase();
+const BackendURL: string = DetermineBackendURL();
+
+function DetermineBackendURL()
+{
+    if (Backend== "dotnet")
+    {
+        return import.meta.env.BACKEND_DOTNET_URL;
+    }
+    else if (Backend == "springboot")
+    {
+        return import.meta.env.BACKEND_SPRING_URL;
+    }
+    else
+    {
+        throw Error(`Incorrect ACTIVE_BACKEND found in .env: ${Backend}`);
+    }
+}
+
+export function GetBackend() { return Backend; }
+export function GetBackendURL() { return BackendURL; }
