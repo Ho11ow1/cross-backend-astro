4d96595ac7b02d479095276c8a8dc98cc237540a
Hollow1 <113149993+Ho11ow1@users.noreply.github.com>
2025-09-07 18:37:49 +0200

    Improve type annotation & backend testing (#5)
    * New Async tests
    
    * temp
    
    * Backend testing

--- AstroFrontend/src/data/Fetch.ts
+++ AstroFrontend/src/data/Fetch.ts
@@ -1,4 +1,5 @@
 import { GetApiEndpoint } from "./AstroDefs";
+import type { User } from "./Types.ts";
 
 export async function GetHelloFor(name: string) : Promise<string>
 {
@@ -13,4 +14,43 @@ export async function GetHelloFor(name: string) : Promise<string>
     const message = data.message;
 
     return message;
+}
+
+export async function GetAllUsers() : Promise<User[] | null>
+{
+    const response = await fetch(`${GetApiEndpoint()}/users`, { method: "GET" });
+    if (!response.ok)
+    {
+        throw new Error(`Error fetching data: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    return data as User[] | null;
+}
+
+export async function GetUserById(id: number) : Promise<User | null>
+{
+    const response = await fetch(`${GetApiEndpoint()}/user/${id}`, { method: "GET" });
+    if (!response.ok)
+    {
+        throw new Error(`Error fetching data: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    return data as User | null;
+}
+
+export async function DeleteUserById(id: number) : Promise<boolean>
+{
+    const response = await fetch(`${GetApiEndpoint()}/users/${id}`, { method: "DELETE" });
+    if (!response.ok)
+    {
+        throw new Error(`Error fetching data: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    return data.success as boolean;
 }
